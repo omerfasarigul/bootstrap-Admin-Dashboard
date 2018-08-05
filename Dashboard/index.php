64a12948ebbf8bbc49af9100d6d3c410391ab49f
omerfasarigul <omerfasarigul@gmail.com>
2018-08-05 03:21:44 +0300

    html code eklemesi yapıldı

--- Dashboard/index.php
+++ Dashboard/index.php
@@ -2,4 +2,20 @@
 
 echo "nettaweb deneme";
 
+
+
  ?>
+<!DOCTYPE html>
+<html lang="tr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+
+    <script type="text/javascript">
+      alert("NABER");
+    </script>
+  </body>
+</html>
